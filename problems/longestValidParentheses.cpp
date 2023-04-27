4812adee206586f4bf75497ff4bb5ddce791dc59
maryamosama33 <maryamosama3383@gmail.com>
2023-04-27 02:43:44 +0200

    Create longestValidParentheses.cpp

---
+++ problems/longestValidParentheses.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include"Stack.cpp"
+using namespace std;
+
+int longestValidParentheses(string s) {
+    Stack<int> stk;
+    int maxLen = 0;
+    stk.push(-1);
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            stk.push(i);
+        } else {
+            stk.pop();
+            if (stk.isEmpty()) {
+                stk.push(i);
+            } else {
+                maxLen = max(maxLen, i - stk.top());
+            }
+        }
+    }
+    return maxLen;
+}
+int main() {
+    string s = "(()";
+    cout << longestValidParentheses(s) << endl;
+}
