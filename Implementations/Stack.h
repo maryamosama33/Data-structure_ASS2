fbe21835ea7ed63a99d79d66c8e2d53be3ab7061
Shahd Mostafa <mostafashahd30@gmail.com>
2023-05-01 00:25:08 +0300

    added stack header

--- Implementations/Stack.h
+++ Implementations/Stack.h
@@ -42,5 +42,86 @@ public:
 
 };
 
+template<typename T>
+bool Stack<T>::isEmpty() {
+    return head == nullptr;
+}
+
+template<typename T>
+void Stack<T>::push(T val) {
+
+    Node<T> *newNode = new Node<T>();
+    newNode->value = val;
+
+    if (head == nullptr){
+        head = newNode;
+    }
+
+    else{
+        newNode->next = head;
+        head = newNode;
+    }
+    size++;
+}
+
+template<typename T>
+T Stack<T>::pop() {
+
+    T nodeVal = head->value;
+
+    if (size == 1){
+        free(head);
+        head = nullptr;
+    }
+    else{
+        Node<T> *temp = head;
+        head = head->next;
+        free(temp);
+    }
+    size--;
+    return nodeVal;
+}
+
+template<typename T>
+void Stack<T>::print() {
+
+    Node<T> *temp = head;
+    while (temp != nullptr){
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+template<typename T>
+T Stack<T>::top() {
+    if (isEmpty()) cout << "This Stack is empty" << endl;
+    else{
+        return head->value;
+    }
+}
+
+template<typename T>
+int Stack<T>::stackSize() {
+    return size;
+}
+
+template<typename T>
+void Stack<T>::clear() {
+
+    if (head != nullptr) {
+        Node<T> temp = new Node<T>();
+        while (head != nullptr) {
+            temp = head;
+            head = head->next;
+            free(temp);
+        }
+        size = 0;
+        cout << "Stack has been cleared!" << endl;
+
+    }
+}
+
+
 
 #endif //DS_ASSIGN2_STACK_H
