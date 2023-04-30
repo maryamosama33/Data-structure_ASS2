e4f27a263f6981b8079fe0d8ca380b16f4bcd56c
Shahd Mostafa <mostafashahd30@gmail.com>
2023-05-01 01:33:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,10 +14,59 @@ int main(){
     ///++++++++++++++++++++++Implementations++++++++++++++++++++++///
 
     ////Try array based list
+//    arrayBasedList<int> list(5); // creates a new ArrayList of maximum size 5
+//    list.insert(10); // adds 10 to the end of the list
+//    list.insert(20); // adds 20 to the end of the list
+//    list.insert(30); // adds 30 to the end of the list
+//    list.insertAt(15, 1); // adds 15 at index 1
+//    list.print(); // prints: 10 15 20 30
+//    cout << endl;
+//    list.replaceAt(25, 2); // replaces the element at index 2 with 25
+//    list.print(); // prints: 10 15 25 30
+//    cout << endl;
+//    list.removeAt(3); // removes the element at index 3
+//    list.print(); // prints: 10 15 25
+//    cout << endl;
+//    cout << "List size: " << list.listSize() << endl; // prints: List size: 3
+//    cout << "Maximum list size: " << list.maxListSize() << endl; // prints: Maximum list size: 5
+//    cout << "Is list empty? " << list.isEmpty() << endl; // prints: Is list empty? 0 (false)
+//    cout << "Is list full? " << list.isFull() << endl; // prints: Is list full? 0 (false)
+//    list.clear(); // clears all elements from the list
+//    cout << "Is list empty? " << list.isEmpty() << endl; // prints: Is list empty? 1 (true)
 
     ////Try singly linked list
+//    singleLinkedList<int> list;
+//    cout << "Initial size of list: " << list.linkedListSize() << endl; // Output: 0
+//
+//    list.insertAtHead(3);
+//    list.insertAtHead(2);
+//    list.insertAtHead(1);
+//    list.insertAtTail(4);
+//    list.insertAtTail(5);
+//    cout << "Size of list after adding elements: " << list.linkedListSize() << endl; // Output: 5
+//    list.print(); // Output: 1 2 3 4 5
+//
+//    cout << "Is 3 in the list? " << list.isExist(3) << endl; // Output: 1 (true)
+//    cout << "Is 6 in the list? " << list.isExist(6) << endl; // Output: 0 (false)
+//
+//    list.replaceAt(10, 2);
+//    list.print(); // Output: 1 2 10 4 5
+//
+//    cout << "Is the element at index 2 equal to 10? " << list.isItemAtEqual(10, 2) << endl; // Output: 1 (true)
+//
+//    list.swap(1, 3);
+//    list.print(); // Output: 1 4 10 2 5
+//
+//    list.clear();
+//    cout << "Size of list after clearing: " << list.linkedListSize() << endl; // Output: 0
 
     ////Try doubly linked list
+
+
+
+
+
+
 
     ////Try circular linked list
 //    CircularLinkedList<int> cll;
@@ -99,5 +148,7 @@ int main(){
 //    string postfix = infixToPostfix(infix);
 //    cout << postfix << endl;
 
+
+    ////Try MergeLinkedList
 
 }
